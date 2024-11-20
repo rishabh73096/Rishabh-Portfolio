7abace4b80f2cb06930139341557e6eace1ebdc9
Rishabh tiwari <rishabhtiwari73096@gmail.com>
2024-11-20 10:16:15 +0530

    updated

--- src/component/ProjectSection.jsx
+++ src/component/ProjectSection.jsx
@@ -27,8 +27,12 @@ const ProjectSection = () => {
                 <ProjectCard title="Tastytails" github="https://github.com/rishabh73096/TastyTrail" liveUrl="https://tasty-trails-eight.vercel.app/" img ="/Tastytails.png" desc="React-based food delivery app using Tailwind CSS and Redux for dynamic cart management."/>
                
                 <ProjectCard title="Prescripto" github="https://github.com/rishabh73096/Prescripto-Doctor-Booking-Appointment-System" liveUrl="https://prescripto-doctor-booking-appointment-system.vercel.app/" img ="/docter.png" desc="A seamless doctor booking System with a context-driven API for efficient scheduling."/>
+              
+                <ProjectCard title="Portfolio" github="https://github.com/rishabh73096/Clipwise-Task" liveUrl="https://clipwise-task.vercel.app/" img ="/clipwise.png" desc="A sleek, responsive homepage UI built with React and styled using Tailwind CSS."/>
+                
+                <ProjectCard title="Clipwise-Design" github="https://github.com/rishabh73096/Rishabh-Portfolio" liveUrl="https://rishabh-portfolio-eight.vercel.app/" img ="/portfolio.jpg" desc="Portfolio showcasing my frontend development skills using React and Tailwind CSS."/>
 
-                <ProjectCard title="Portfolio" github="https://github.com/rishabh73096/Rishabh-Portfolio" liveUrl="https://rishabh-portfolio-eight.vercel.app/" img ="/portfolio.jpg" desc="Portfolio showcasing my frontend development skills using React and Tailwind CSS."/>
+               
             </div>
             <div className="flex justify-center items-center gap-2 select-none text-white">
                 <FaArrowLeft onClick={LeftScroll} className="text-xl lg:text-2xl text-yellow-500 hidden lg:block cursor-pointer" />

f0ee74e1799e00f68c07e7b41ea0874ce51922dc
Rishabh-2digit <rishabhtiwari73096@gmail.com>
2025-07-08 23:32:07 +0530

    update

--- src/component/HomeCard.jsx
+++ src/component/HomeCard.jsx
@@ -1,31 +1,46 @@
-
 const HomeCard = () => {
-    return (<>
-        <div className="flex flex-col lg:flex-row gap-5 lg:gap-0 justify-between items-center mb-20 lg:mb-36">
-            <div
-                data-aos="fade-up-right"
-                className="flex flex-col gap-5 lg:w-[50%] text-white">
-
-                <h1 className="text-5xl lg:7xl gradiant-text mb-5 lg:mb-10"> Hey ! </h1>
-                <p className="text-xl lg:text-[19px] text-gray-300">
-                    I am a passionate Front-End Developer with a Master of Computer Applications (MCA), achieving a CGPA of 9.0. My strengths lie in creating dynamic, user-friendly websites and applications using HTML, CSS, JavaScript, and ReactJS.
-                </p>
-                <p className="text-xl lg:text-[19px] text-gray-300">
-                    In addition to front-end technologies, I am familiar with modern CSS frameworks and proficient in both SQL and NoSQL databases, currently expanding my knowledge to include MongoDB. I also have experience with backend technologies like Node.js and Express.js, as well as cloud services like Appwrite and Firebase.
-                </p>
-                <p className="text-xl lg:text-[19px] text-gray-300">
-                    Dedicated to staying updated with industry trends, I aim for excellence in every project, whether working independently or collaboratively, delivering exceptional digital solutions.
-                </p>
-                <a href="https://drive.google.com/file/d/156HcfxrcbfZmeLz9LA8rE1iqOE4wKzuz/view?usp=sharing" className="font-mono text-center font-semibold text-lg lg:text-2xl cursor-pointer mb-3 lg:mb-0 hover:border-2 p-2 hover:border-gray-200 hover:backdrop-blur-lg rounded-xl gradiant-text">See My Resume</a>
-            </div>
-            <div data-aos="fade-up-left">
-
-                <img src="./Profile.jpg "
-                    className="rounded-full w-[230px] h-[230px] lg:w-[450px] lg:h-[450px]" />
-            </div>
+  return (
+    <>
+      <div className="flex flex-col lg:flex-row gap-5 lg:gap-0 justify-between items-center mb-20 lg:mb-36">
+        <div
+          data-aos="fade-up-right"
+          className="flex flex-col gap-5 lg:w-[50%] text-white"
+        >
+          <h1 className="text-5xl lg:7xl gradiant-text mb-5 lg:mb-10">
+            {" "}
+            Hey !{" "}
+          </h1>
+          <p className="text-xl lg:text-[19px] text-gray-300">
+            Hi, I'm Rishabh Tiwari — a Full Stack Developer with 2 years of
+            combined freelance and professional experience. I specialize in
+            building end-to-end web applications using the MERN stack, and I
+            actively contribute to both frontend and backend architecture.
+          </p>
+          <p className="text-xl lg:text-[19px] text-gray-300">
+            Beyond development, I work with Figma to design user-centric
+            interfaces, and I have also implemented DevOps practices like CI/CD
+            pipelines, containerization with Docker, and cloud deployments to
+            streamline delivery and scale effectively.
+          </p>
+          <p className="text-xl lg:text-[19px] text-gray-300">
+            I enjoy building meaningful tech products that solve real problems,
+            and I am always open to new challenges and collaborations.
+          </p>
+          <a
+            href="https://drive.google.com/file/d/1LN55QJpFfm4gWsyWe4vl3YvZWJWcJw2E/view?usp=sharing"
+            className="font-mono text-center font-semibold text-lg lg:text-2xl cursor-pointer mb-3 lg:mb-0 hover:border-2 p-2 hover:border-gray-200 hover:backdrop-blur-lg rounded-xl gradiant-text"
+          >
+            See My Resume
+          </a>
         </div>
-
-
-    </>)
-}
-export default HomeCard
+        <div data-aos="fade-up-left">
+          <img
+            src="./Profile.jpg "
+            className="rounded-full w-[230px] h-[230px] lg:w-[450px] lg:h-[450px]"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+export default HomeCard;
--- src/component/ProjectSection.jsx
+++ src/component/ProjectSection.jsx
@@ -4,79 +4,119 @@ import { FaArrowLeft } from "react-icons/fa6";
 import { useRef } from "react";
 
 const ProjectSection = () => {
+  const scrollRef = useRef();
+  const LeftScroll = () => {
+    scrollRef.current.scrollLeft -= 500;
+  };
+  const rightScroll = () => {
+    scrollRef.current.scrollLeft += 500;
+  };
 
-    const scrollRef = useRef();
-    const LeftScroll = () => {
-        scrollRef.current.scrollLeft -= 500;
-    }
-    const rightScroll = () => {
-        scrollRef.current.scrollLeft += 500;
-    }
+  const projects = [
+    {
+      title: "Ecommerce Store",
+      github: "https://github.com/jasznco/jasznco-web",
+      liveUrl: "https://main.d29ub4hxfp2gu3.amplifyapp.com/",
+      img: "/image2.png",
+      desc: "A responsive ecommerce site for browsing, filtering, and securely buying products",
+    },
+    {
+      title: "My Lodge",
+      github: "",
+      liveUrl: "https://www.mylodge.cloud/",
+      img: "/image3.png",
+      desc: "A rental platform where users can book lodges and hosts can list their properties.",
+    },
+    {
+      title: "GroceryPickup Store",
+      github: "https://github.com/TwoDigitInnovations/grocerypickup-web",
+      liveUrl: "https://www.bachhoahouston.com/",
+       img: "/image1.png",
+      desc: "A grocery web app for browsing items, adding to cart, and scheduling in-store pickups.",
+    },
+    {
+      title: "Hariir (Job Portal)",
+      github: "https://github.com/TwoDigitInnovations/hariir-web",
+      liveUrl: "https://hariir-web.vercel.app/",
+       img: "/image4.png",
+      desc: "A social app for connecting professionals, Company, and managing personal portfolios.",
+    },
 
-    const projects = [
-        {
-            title: "Threads Of Artisans",
-            github: "https://github.com/rishabh73096/threads-of-Artisans-frontend",
-            liveUrl: "https://threads-of-artisans-frontend.vercel.app/",
-            img: "/Threads-of-Artisans.jpg",
-            desc: "A MERN stack eCommerce platform connecting artisans to global customers.",
-        },
-        {
-            title: "Tastytails",
-            github: "https://github.com/rishabh73096/TastyTrail",
-            liveUrl: "https://tasty-trails-eight.vercel.app",
-            img: "/Tastytails.png",
-            desc: "React-based food delivery app using Tailwind CSS and Redux for dynamic cart management.",
-        },
-        {
-            title: "Prescripto",
-            github: "https://github.com/rishabh73096/Prescripto-Doctor-Booking-Appointment-System",
-            liveUrl: "https://prescripto-doctor-booking-appointment-system.vercel.app",
-            img: "/docter.png",
-            desc: "A seamless doctor booking System with a context-driven API for efficient scheduling.",
-        },
-        {
-            title: "Clipwise-Design",
-            github: "https://github.com/rishabh73096/Clipwise-Task",
-            liveUrl: "https://clipwise-task.vercel.app",
-            img: "/clipwise.png",
-            desc: "A sleek, responsive homepage UI built with React and styled using Tailwind CSS.",
-        },
-        {
-            title: " Portfolio",
-            github: "https://github.com/rishabh73096/Rishabh-Portfolio",
-            liveUrl: "https://rishabh-portfolio-fawn.vercel.app",
-            img: "/portfolio.jpg",
-            desc: "Portfolio showcasing my frontend development skills using React and Tailwind CSS.",
-        },
-    ];
+    {
+      title: "Prescripto",
+      github:
+        "https://github.com/rishabh73096/Prescripto-Doctor-Booking-Appointment-System",
+      liveUrl:
+        "https://prescripto-doctor-booking-appointment-system.vercel.app",
+      img: "/docter.png",
+      desc: "A seamless doctor booking System with a context-driven API for efficient scheduling.",
+    },
+    {
+      title: "Threads Of Artisans",
+      github: "https://github.com/rishabh73096/threads-of-Artisans-frontend",
+      liveUrl: "https://threads-of-artisans-frontend.vercel.app/",
+      img: "/Threads-of-Artisans.jpg",
+      desc: "A MERN stack eCommerce platform connecting artisans to global customers.",
+    },
+    {
+      title: "Clipwise-Design",
+      github: "https://github.com/rishabh73096/Clipwise-Task",
+      liveUrl: "https://clipwise-task.vercel.app",
+      img: "/clipwise.png",
+      desc: "A sleek, responsive homepage UI built with React and styled using Tailwind CSS.",
+    },
+    {
+      title: " Portfolio",
+      github: "https://github.com/rishabh73096/Rishabh-Portfolio",
+      liveUrl: "https://rishabh-portfolio-fawn.vercel.app",
+      img: "/portfolio.jpg",
+      desc: "Portfolio showcasing my frontend development skills using React and Tailwind CSS.",
+    },
+    {
+      title: "Tastytails",
+      github: "https://github.com/rishabh73096/TastyTrail",
+      liveUrl: "https://tasty-trails-eight.vercel.app",
+      img: "/Tastytails.png",
+      desc: "React-based food delivery app using Tailwind CSS and Redux for dynamic cart management.",
+    },
+  ];
 
-    return (<>
+  return (
+    <>
+      <div data-aos="fade-down" className="mb-20 lg:mb-36">
+        <h1 className="text-5xl lg:text-7xl mb-10 gradiant-text">
+          Project section{" "}
+        </h1>
         <div
-            data-aos="fade-down"
-            className="mb-20 lg:mb-36">
-            <h1 className="text-5xl lg:text-7xl mb-10 gradiant-text">Project section </h1>
-            <div ref={scrollRef} className="flex overflow-x-scroll gap-5 scroll-hide">
-                {projects.map((project, index) => (
-                    <ProjectCard
-                        key={index}
-                        title={project.title}
-                        github={project.github}
-                        liveUrl={project.liveUrl}
-                        img={project.img}
-                        desc={project.desc}
-                    />
-                ))}
-            </div>
-            <div className="flex justify-center items-center gap-2 select-none text-white">
-                <FaArrowLeft onClick={LeftScroll} className="text-xl lg:text-2xl text-yellow-500 hidden lg:block cursor-pointer" />
-                <h2 className=" gradiant-text font-mono text-lg uppercase">Slide for more </h2>
-                <FaArrowRight onClick={rightScroll} className="text-xl lg:text-2xl text-yellow-500 hidden lg:block cursor-pointer" />
-            </div>
-
-
+          ref={scrollRef}
+          className="flex overflow-x-scroll gap-5 scroll-hide"
+        >
+          {projects.map((project, index) => (
+            <ProjectCard
+              key={index}
+              title={project.title}
+              github={project.github}
+              liveUrl={project.liveUrl}
+              img={project.img}
+              desc={project.desc}
+            />
+          ))}
         </div>
-    </>)
-
-}
+        <div className="flex justify-center items-center gap-2 select-none text-white">
+          <FaArrowLeft
+            onClick={LeftScroll}
+            className="text-xl lg:text-2xl text-yellow-500 hidden lg:block cursor-pointer"
+          />
+          <h2 className=" gradiant-text font-mono text-lg uppercase">
+            Slide for more{" "}
+          </h2>
+          <FaArrowRight
+            onClick={rightScroll}
+            className="text-xl lg:text-2xl text-yellow-500 hidden lg:block cursor-pointer"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
 export default ProjectSection;
